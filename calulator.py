e3f49425a1d473b2487410e0759bac52bb18a122
Shashidhar Pachimatla <pachimatlashashidhar523@gmail.com>
2025-03-08 14:32:06 +0530

    Add files via upload

---
+++ calulator.py
@@ -0,0 +1,11 @@
+num1=float(input("Enter first number:"))
+num2=float(input("Enter a second number:"))
+add=num1+num2
+sub=num1-num2
+mult=num1*num2
+div=num1/num2
+print("\n")
+print("Addition:"+str(add))
+print("Substraction:"+str(sub))
+print("Multiplication:"+str(mult))
+print("Division:"+str(div))
